ac09d0d97b2a87345b8471d5ea0bab19473ab973
dorichan <dori.p.chan@gmail.com>
2015-03-15 22:30:44 -0700

    -initial commit

---
+++ DataStructures/BinarySearch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void CreateArray()
+        {
+            int[] bsArray = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+            foreach (var i in bsArray)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CreateArray();
+
+            var userInput = Console.ReadLine();
+        }
+    }
+}
