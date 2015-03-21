68530aa3810b37f23530cd77e4eeeaad0198d486
dorichan <dori.p.chan@gmail.com>
2015-03-20 19:14:19 -0700

    - Implementing a recursive sorting method

--- DataStructures/BinarySearch/Program.cs
+++ DataStructures/BinarySearch/Program.cs
@@ -8,39 +8,70 @@ namespace BinarySearch
 {
     class Program
     {
-        static int[] CreateArray()
+		static int[] CreateArray(int _num)
         {
-            int[] bsArray = new int[16];
+            int[] array = new int[_num];
             Random random = new Random();
 
-            for (int i = 0; i <= 15; i++)
-            {
-                bsArray[i] = random.Next(1, 150);
+            for (int i = 0; i < _num; i++) {
+                array[i] = random.Next(1, 150);
             }
             
-            return bsArray;
+            return array;
         }
 
-        static void SortArray()
+		// TODO Continue work on this. Make it recursive. Possibly a quick sort instead of just meh.
+		static int[] Sort(int[] _array)
         {
+			int index = 0;
+			int current = _array [index];
+			int next = _array [index+1];
 
+			for (int i = 0; i < _array.Length; i++) {
+				if (current < next) {
+					Console.WriteLine (current + " is less than " + next + " ... so swapping.");
+					int temp = current;
+					current = next;
+					next = temp;
+					index += 1;
+					return Sort (_array);
+				}
+			}
+
+			return _array;
         }
 
-        static void SearchArray()
+		static void BSearch(int[] _array, int _tValue)
         {
 
         }
 
         static void Main(string[] args)
         {
-            int[] testArray = CreateArray();
+			Console.WriteLine ("How big should the array be: ");
+			var numOfInts = Console.ReadLine ();
+			int num = Convert.ToInt32 (numOfInts);
+			int[] testArray = CreateArray(num);
 
-            foreach (var i in testArray)
-            {
-                Console.WriteLine(i);
+			for (int i = 0; i < testArray.Length; i++) {
+				Console.WriteLine("Old Pos " + i + ": " + testArray[i]);
+			}
+
+			int[] sortedArray = Sort (testArray);
+
+			for (int i = 0; i < sortedArray.Length; i++) {
+				Console.WriteLine("Pos " + i + ": " + sortedArray[i]);
             }
 
-            var userInput = Console.ReadLine();
+			// Query the user for input to search the array for.
+			Console.WriteLine ("\nEnter a number to search for: \n");
+
+			var input = Console.ReadLine ();
+			if (input != null) {
+				BSearch (sortedArray, Convert.ToInt32 (input));
+			}
+
+			// Add a check to see if user input is an int
         }
     }
 }
